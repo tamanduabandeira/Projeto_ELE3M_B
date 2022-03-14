d295e36ac2e07441fa6fc78c47808fe95e26752d
JOSÉ WILLIAM RODRIGUES PEREIRA <sn1011701@cfp127aluno.local>
2022-03-14 09:55:08 -0300

    Criação do projeto de firmware.

---
+++ mplabX.X/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: sn1011701
+ *
+ * Created on 14 de Março de 2022, 09:52
+ */
+
+#include "config.h"
+#include <xc.h>
+
+void main(void) 
+{
+
+    while( 1 )
+    {
+    
+    }
+}
